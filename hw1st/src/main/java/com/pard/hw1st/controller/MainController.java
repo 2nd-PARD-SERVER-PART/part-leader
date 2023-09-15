6f864bd6283db37f4867990c76b5e9539e4e75ef
ParkJiSung <gggg0195@gmail.com>
2023-09-16 03:00:12 +0900

    hw1st 추가

---
+++ hw1st/src/main/java/com/pard/hw1st/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.pard.hw1st.controller;
+
+
+// TODO: MainController를 만들어주세요.
+
+public class MainController {
+
+    public String startPage() {
+        return "";
+    }
+
+
+    public String showForm() {
+
+        return "";
+    }
+
+    public String newInfoForm() {
+        return "";
+    }
+
+}
